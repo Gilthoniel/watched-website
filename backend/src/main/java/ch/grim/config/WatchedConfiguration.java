525f0d10c0687de5e6fb74b078bc8fcf6608999a
Gaylor Bosson <gaylor.bosson@gmail.com>
2016-10-05 15:06:57 +0200

    update(cache): modify ttl of entries

--- backend/src/main/java/ch/grim/config/WatchedConfiguration.java
+++ backend/src/main/java/ch/grim/config/WatchedConfiguration.java
@@ -5,7 +5,14 @@ import ch.grim.models.Series;
 import ch.grim.serializers.MovieSerializer;
 import ch.grim.serializers.SeriesSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.interceptor.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -13,6 +20,7 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Gaylor on 31.07.2016.
@@ -20,7 +28,10 @@ import java.util.List;
  */
 @Configuration
 @EnableCaching
-public class WatchedConfiguration extends WebMvcConfigurerAdapter {
+public class WatchedConfiguration extends WebMvcConfigurerAdapter implements CachingConfigurer {
+
+    private static final int MAX_NUMBER_ENTRIES = 1000000;
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         SimpleModule m = new SimpleModule();
@@ -30,5 +41,34 @@ public class WatchedConfiguration extends WebMvcConfigurerAdapter {
 
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder().modules(m);
         converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+    }
+
+    @Override
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager() {
+            @Override
+            protected Cache createConcurrentMapCache(final String name) {
+                return new ConcurrentMapCache(name,
+                        CacheBuilder.newBuilder()
+                                .expireAfterWrite(24, TimeUnit.HOURS)
+                                .maximumSize(MAX_NUMBER_ENTRIES)
+                                .build().asMap(), false);
+            }
+        };
+    }
+
+    @Override
+    public CacheResolver cacheResolver() {
+        return new SimpleCacheResolver();
+    }
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new SimpleKeyGenerator();
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new SimpleCacheErrorHandler();
     }
 }
--- frontend/run_package.sh
+++ frontend/run_package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -jar -Dspring.profiles.active=production server/package.jar > logs.out 2> logs.err &
+nohup java -jar -Dspring.profiles.active=production server/package.jar &
