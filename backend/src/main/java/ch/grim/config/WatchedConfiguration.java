f30470549356a36598388d3ebf97747ef7c15bd7
Gaylor Bosson <gaylor.bosson@gmail.com>
2016-10-05 15:22:34 +0200

    revert cache

--- backend/src/main/java/ch/grim/config/WatchedConfiguration.java
+++ backend/src/main/java/ch/grim/config/WatchedConfiguration.java
@@ -5,14 +5,7 @@ import ch.grim.models.Series;
 import ch.grim.serializers.MovieSerializer;
 import ch.grim.serializers.SeriesSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.google.common.cache.CacheBuilder;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.cache.interceptor.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -20,7 +13,6 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Gaylor on 31.07.2016.
@@ -28,7 +20,7 @@ import java.util.concurrent.TimeUnit;
  */
 @Configuration
 @EnableCaching
-public class WatchedConfiguration extends WebMvcConfigurerAdapter implements CachingConfigurer {
+public class WatchedConfiguration extends WebMvcConfigurerAdapter {
 
     private static final int MAX_NUMBER_ENTRIES = 1000000;
 
@@ -41,34 +33,5 @@ public class WatchedConfiguration extends WebMvcConfigurerAdapter implements Cac
 
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder().modules(m);
         converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
-    }
-
-    @Override
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager() {
-            @Override
-            protected Cache createConcurrentMapCache(final String name) {
-                return new ConcurrentMapCache(name,
-                        CacheBuilder.newBuilder()
-                                .expireAfterWrite(24, TimeUnit.HOURS)
-                                .maximumSize(MAX_NUMBER_ENTRIES)
-                                .build().asMap(), false);
-            }
-        };
-    }
-
-    @Override
-    public CacheResolver cacheResolver() {
-        return new SimpleCacheResolver();
-    }
-
-    @Override
-    public KeyGenerator keyGenerator() {
-        return new SimpleKeyGenerator();
-    }
-
-    @Override
-    public CacheErrorHandler errorHandler() {
-        return new SimpleCacheErrorHandler();
     }
 }
